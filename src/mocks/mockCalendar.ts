a3e65c5e59a5770ade3456f7eb9850b54e413197
Administrador <Marcelo Cardoso>
2021-05-13 12:00:10 -0300

    Mock de calendário alterado

--- src/mocks/mockCalendar.ts
+++ src/mocks/mockCalendar.ts
@@ -26,33 +26,8 @@ export const calendar: ICalendar[] = [
             },
             {
                 name: 'Unidade Praça Osório',
-                turn: 'Manhã',
-                day: '3ª Feira'
-            },
-            {
-                name: 'Unidade Praça Osório',
                 turn: 'Noite',
                 day: '5ª Feira'
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Manhã',
-                day: '4ª Feira',
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Noite',
-                day: '6ª Feira',
-            },
-            {
-                name: 'Campus Londrina',
-                turn: 'Manhã',
-                day: '2ª Feira'
-            },
-            {
-                name: 'Campus Londrina',
-                turn: 'Noite',
-                day: '4ª Feira'
             }
         ]
     },
@@ -70,31 +45,6 @@ export const calendar: ICalendar[] = [
                 day: '6ª Feira'
             },
             {
-                name: 'Unidade Praça Osório',
-                turn: 'Manhã',
-                day: '4ª Feira'
-            },
-            {
-                name: 'Unidade Praça Osório',
-                turn: 'Noite',
-                day: '4ª Feira'
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Manhã',
-                day: '2ª Feira'
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Noite',
-                day: '3ª Feira'
-            },
-            {
-                name: 'Campus Londrina',
-                turn: 'Manhã',
-                day: '3ª Feira'
-            },
-            {
                 name: 'Campus Londrina',
                 turn: 'Noite',
                 day: '6ª Feira'
@@ -106,40 +56,6 @@ export const calendar: ICalendar[] = [
         local: [
             {
                 name: 'Campus Ecoville',
-                turn: 'Manhã',
-                day: '4ª Feira'
-            },
-            {
-                name: 'Campus Ecoville',
-                turn: 'Noite',
-                day: '2ª Feira'
-            },{
-                name: 'Unidade Praça Osório',
-                turn: 'Manhã',
-                day: '5ª Feira'
-            },
-            {
-                name: 'Unidade Praça Osório',
-                turn: 'Noite',
-                day: '3ª Feira'
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Manhã',
-                day: '6ª Feira'
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Noite',
-                day: '5ª Feira'
-            },
-            {
-                name: 'Campus Londrina',
-                turn: 'Manhã',
-                day: '4ª Feira'
-            },
-            {
-                name: 'Campus Londrina',
                 turn: 'Noite',
                 day: '2ª Feira'
             }
@@ -148,36 +64,6 @@ export const calendar: ICalendar[] = [
     {
         categoryId: '5f866de9ffabe94b908e42ad',
         local: [
-            {
-                name: 'Campus Ecoville',
-                turn: 'Manhã',
-                day: '5ª Feira'
-            },
-            {
-                name: 'Campus Ecoville',
-                turn: 'Noite',
-                day: '3ª Feira'
-            },
-            {
-                name: 'Unidade Praça Osório',
-                turn: 'Manhã',
-                day: '6ª Feira'
-            },
-            {
-                name: 'Unidade Praça Osório',
-                turn: 'Noite',
-                day: '2ª Feira'
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Manhã',
-                day: '3ª Feira'
-            },
-            {
-                name: 'Unidade Santos Andrade',
-                turn: 'Noite',
-                day: '4ª Feira'
-            },
             {
                 name: 'Campus Londrina',
                 turn: 'Manhã',
